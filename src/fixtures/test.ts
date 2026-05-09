6138170a39feb59e2299a3ddd610a733f7a93a55
Diego Cortes <cortesroadiegoalejandro@gmail.com>
2026-05-09 15:56:46 -0500

    feat(fixtures): add test fixture injecting all pages

---
+++ src/fixtures/test.ts
@@ -0,0 +1,27 @@
+import { test as base, expect } from '@playwright/test';
+import { LoginPage } from '@pages/LoginPage';
+import { InventoryPage } from '@pages/InventoryPage';
+import { CartPage } from '@pages/CartPage';
+import { CheckoutInfoPage } from '@pages/checkout/CheckoutInfoPage';
+import { CheckoutOverviewPage } from '@pages/checkout/CheckoutOverviewPage';
+import { CheckoutCompletePage } from '@pages/checkout/CheckoutCompletePage';
+
+type Pages = {
+  loginPage: LoginPage;
+  inventoryPage: InventoryPage;
+  cartPage: CartPage;
+  checkoutInfoPage: CheckoutInfoPage;
+  checkoutOverviewPage: CheckoutOverviewPage;
+  checkoutCompletePage: CheckoutCompletePage;
+};
+
+export const test = base.extend<Pages>({
+  loginPage: async ({ page }, use) => use(new LoginPage(page)),
+  inventoryPage: async ({ page }, use) => use(new InventoryPage(page)),
+  cartPage: async ({ page }, use) => use(new CartPage(page)),
+  checkoutInfoPage: async ({ page }, use) => use(new CheckoutInfoPage(page)),
+  checkoutOverviewPage: async ({ page }, use) => use(new CheckoutOverviewPage(page)),
+  checkoutCompletePage: async ({ page }, use) => use(new CheckoutCompletePage(page)),
+});
+
+export { expect };
